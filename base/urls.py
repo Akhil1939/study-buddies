f2b56f680075fcd91bd99af2dbd4b5c49e69800c
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2024-03-05 22:10:50 +0530

    add : auth and room setup

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('login/', views.loginPage, name='login'),
+    path('logout/' , views.logoutUser, name='logout'), 
+    path('register/' , views.registerPage, name='register'), 
+    path('', views.home, name="home"),
+    path('room/create/', views.createRoom, name="create-room"),
+    path('room/update/<str:pk>', views.updateRoom, name="update-room"),
+    path('room/delete/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('room/message/delete/<str:pk>/', views.deleteMessage, name="delete-room-message"),
+    path('room/<str:pk>/', views.room, name="room"),
+]
